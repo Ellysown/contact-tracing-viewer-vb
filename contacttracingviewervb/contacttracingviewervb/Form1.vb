cdece80ddbadc7671965fa735f602bc06e16be2f
Ellyson <xxLAxxjeje09@gmail.com>
2022-06-25 02:01:44 +0800

    Added If Else function on Insert File name and close button

--- contacttracingviewervb/contacttracingviewervb/Form1.vb
+++ contacttracingviewervb/contacttracingviewervb/Form1.vb
@@ -5,10 +5,19 @@
 
     Private Sub viewbtn_Click(sender As Object, e As EventArgs) Handles viewbtn.Click
         Dim Reader As System.IO.StreamReader
-
         Dim insertname As String
         insertname = namebox1.Text + ".txt"
-        Reader = My.Computer.FileSystem.OpenTextFileReader("D:\School chuchu\ELECS 3\" + insertname)
-        richtxtbx1.Text = Reader.ReadToEnd
+
+        If System.IO.File.Exists("D:\School chuchu\ELECS 3\" + insertname) Then
+            Reader = My.Computer.FileSystem.OpenTextFileReader("D:\School chuchu\ELECS 3\" + insertname)
+            richtxtbx1.Text = Reader.ReadToEnd
+        Else
+            MessageBox.Show("File Does Not Exist!")
+        End If
+
+    End Sub
+
+    Private Sub closebtn_Click(sender As Object, e As EventArgs) Handles closebtn.Click
+        Close()
     End Sub
 End Class
