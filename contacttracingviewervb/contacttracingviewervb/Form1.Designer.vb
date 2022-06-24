8578ede4e97626dcb60d9b3ba08fd4bb31fbad93
Ellyson <xxLAxxjeje09@gmail.com>
2022-06-25 01:50:35 +0800

    Added Insert File Name Function and View Function

--- contacttracingviewervb/contacttracingviewervb/Form1.Designer.vb
+++ contacttracingviewervb/contacttracingviewervb/Form1.Designer.vb
@@ -23,13 +23,13 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim Title As System.Windows.Forms.Label
-        Dim Readfile As System.Windows.Forms.Label
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Dim insert As System.Windows.Forms.Label
+        Me.viewbtn = New System.Windows.Forms.Button()
+        Me.closebtn = New System.Windows.Forms.Button()
+        Me.richtxtbx1 = New System.Windows.Forms.RichTextBox()
         Me.namebox1 = New System.Windows.Forms.TextBox()
         Title = New System.Windows.Forms.Label()
-        Readfile = New System.Windows.Forms.Label()
+        insert = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Title
@@ -43,41 +43,41 @@ Partial Class Form1
         Title.Text = "Contact Tracing Viewer"
         AddHandler Title.Click, AddressOf Me.Title_Click
         '
-        'Button1
+        'insert
         '
-        Me.Button1.Location = New System.Drawing.Point(388, 487)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 7
-        Me.Button1.Text = "View"
-        Me.Button1.UseVisualStyleBackColor = True
+        insert.AutoSize = True
+        insert.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        insert.Location = New System.Drawing.Point(31, 132)
+        insert.Name = "insert"
+        insert.Size = New System.Drawing.Size(196, 29)
+        insert.TabIndex = 10
+        insert.Text = "Insert File Name:"
         '
-        'Button2
+        'viewbtn
         '
-        Me.Button2.Location = New System.Drawing.Point(491, 487)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 8
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.viewbtn.Location = New System.Drawing.Point(388, 487)
+        Me.viewbtn.Name = "viewbtn"
+        Me.viewbtn.Size = New System.Drawing.Size(75, 23)
+        Me.viewbtn.TabIndex = 7
+        Me.viewbtn.Text = "View"
+        Me.viewbtn.UseVisualStyleBackColor = True
         '
-        'RichTextBox1
+        'closebtn
         '
-        Me.RichTextBox1.Location = New System.Drawing.Point(36, 164)
-        Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(530, 301)
-        Me.RichTextBox1.TabIndex = 9
-        Me.RichTextBox1.Text = ""
+        Me.closebtn.Location = New System.Drawing.Point(491, 487)
+        Me.closebtn.Name = "closebtn"
+        Me.closebtn.Size = New System.Drawing.Size(75, 23)
+        Me.closebtn.TabIndex = 8
+        Me.closebtn.Text = "Close"
+        Me.closebtn.UseVisualStyleBackColor = True
         '
-        'Readfile
+        'richtxtbx1
         '
-        Readfile.AutoSize = True
-        Readfile.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Readfile.Location = New System.Drawing.Point(31, 132)
-        Readfile.Name = "Readfile"
-        Readfile.Size = New System.Drawing.Size(196, 29)
-        Readfile.TabIndex = 10
-        Readfile.Text = "Insert File Name:"
+        Me.richtxtbx1.Location = New System.Drawing.Point(36, 164)
+        Me.richtxtbx1.Name = "richtxtbx1"
+        Me.richtxtbx1.Size = New System.Drawing.Size(530, 301)
+        Me.richtxtbx1.TabIndex = 9
+        Me.richtxtbx1.Text = ""
         '
         'namebox1
         '
@@ -93,10 +93,10 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(605, 559)
         Me.Controls.Add(Me.namebox1)
-        Me.Controls.Add(Readfile)
-        Me.Controls.Add(Me.RichTextBox1)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(insert)
+        Me.Controls.Add(Me.richtxtbx1)
+        Me.Controls.Add(Me.closebtn)
+        Me.Controls.Add(Me.viewbtn)
         Me.Controls.Add(Title)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -105,8 +105,8 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents viewbtn As Button
+    Friend WithEvents closebtn As Button
+    Friend WithEvents richtxtbx1 As RichTextBox
     Friend WithEvents namebox1 As TextBox
 End Class
--- contacttracingviewervb/contacttracingviewervb/Form1.vb
+++ contacttracingviewervb/contacttracingviewervb/Form1.vb
@@ -2,4 +2,13 @@
     Private Sub Title_Click(sender As Object, e As EventArgs)
 
     End Sub
+
+    Private Sub viewbtn_Click(sender As Object, e As EventArgs) Handles viewbtn.Click
+        Dim Reader As System.IO.StreamReader
+
+        Dim insertname As String
+        insertname = namebox1.Text + ".txt"
+        Reader = My.Computer.FileSystem.OpenTextFileReader("D:\School chuchu\ELECS 3\" + insertname)
+        richtxtbx1.Text = Reader.ReadToEnd
+    End Sub
 End Class
